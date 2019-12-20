b2b1535382c4c3d09295e439c6d79d1934f7b869
AJ <aj.fernando@holoplot.com>
2019-12-20 16:13:47 +0100

    Vue add typescript
    adds typescript to already configured project

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import vb from "vue-babylonjs";
+import App from "./App.vue";
+
+Vue.use(vb);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
