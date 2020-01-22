5e6188a8eb4d649acaaaa18e4e7982131c30abcf
AJ <aj.fernando@holoplot.com>
2020-01-22 11:44:29 +0100

    npm run lint

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
   <div id="app">
     <Box> </Box>
-    <HelloWorld msg="3D view with Vue+Babylonjs"/>
-    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="3D view with Vue+Babylonjs" />
+    <img alt="Vue logo" src="./assets/logo.png" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Box from './components/Box.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Box from "./components/Box.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld,
     Box
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
